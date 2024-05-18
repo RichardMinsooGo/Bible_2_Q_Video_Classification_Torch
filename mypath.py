510153c78f72f89703ebcfec75c786a2d94d181a
Richard Minsoo Go <kauntach@naver.com>
2024-05-18 10:48:28 +0900

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -6,12 +6,12 @@ class Path(object):
             root_dir = '/content/dataset/UCF-101'
 
             # Save preprocess data into output_dir
-            output_dir = '/content/VAR/ucf101'
+            output_dir = '/content/content/VAR/ucf101'
 
             return root_dir, output_dir
         elif database == 'hmdb51':
             # folder that contains class labels
-            root_dir = '/contento/hmdb-51'
+            root_dir = '/content/hmdb-51'
 
             output_dir = '/content/VAR/hmdb51'
 
