6c917788105c806cfdea79323763f1047a6c1b0a
Richard Minsoo Go <kauntach@naver.com>
2024-05-18 10:55:49 +0900

    Update train.py

--- train.py
+++ train.py
@@ -18,12 +18,12 @@ from network import C3D_model, R2Plus1D_model, R3D_model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Device being used:", device)
 
-nEpochs = 100  # Number of epochs for training
-resume_epoch = 0  # Default is 0, change if want to resume
-useTest = True # See evolution of the test set when training
-nTestInterval = 20 # Run on test set every nTestInterval epochs
-snapshot = 50 # Store a model every snapshot epochs
-lr = 1e-3 # Learning rate
+nEpochs       = 100    # Number of epochs for training
+resume_epoch  = 0      # Default is 0, change if want to resume
+useTest       = True  # See evolution of the test set when training
+nTestInterval = 20     # Run on test set every nTestInterval epochs
+snapshot      = 50     # Store a model every snapshot epochs
+lr            = 1e-3   # Learning rate
 
 dataset = 'ucf101' # Options: hmdb51 or ucf101
 
@@ -93,10 +93,11 @@ def train_model(dataset=dataset, save_dir=save_dir, num_classes=num_classes, lr=
     log_dir = os.path.join(save_dir, 'models', datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname())
     writer = SummaryWriter(log_dir=log_dir)
 
+    
     print('Training model on {} dataset...'.format(dataset))
-    train_dataloader = DataLoader(VideoDataset(dataset=dataset, split='train',clip_len=16), batch_size=20, shuffle=True, num_workers=4)
-    val_dataloader   = DataLoader(VideoDataset(dataset=dataset, split='val',  clip_len=16), batch_size=20, num_workers=4)
-    test_dataloader  = DataLoader(VideoDataset(dataset=dataset, split='test', clip_len=16), batch_size=20, num_workers=4)
+    train_dataloader = DataLoader(VideoDataset(dataset=dataset, split='train',clip_len=16, preprocess=False), batch_size=80, shuffle=True, num_workers=4)
+    val_dataloader   = DataLoader(VideoDataset(dataset=dataset, split='val',  clip_len=16, preprocess=False), batch_size=80, num_workers=4)
+    test_dataloader  = DataLoader(VideoDataset(dataset=dataset, split='test', clip_len=16, preprocess=False), batch_size=880, num_workers=4)
 
     trainval_loaders = {'train': train_dataloader, 'val': val_dataloader}
     trainval_sizes = {x: len(trainval_loaders[x].dataset) for x in ['train', 'val']}
